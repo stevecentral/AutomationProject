1987a7534938081855179fffd4113a627e88f397
Stefan Ticu <93165878+stevecentral@users.noreply.github.com>
2025-04-16 16:25:40 -0400

    Update comments

--- script_application.py
+++ script_application.py
@@ -4,9 +4,11 @@ from main_window import Ui_Dialog
 
 class DialogWindow(QtWidgets.QDialog, Ui_Dialog):
     def __init__(self):
+        # Initialize parent classes
         super().__init__()
         self.setupUi(self)
 
+# Main entry point of the application
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     dialog = DialogWindow()
